b6b9f19681d7df9c3b88408a6ffa91aa8c99c091
HeuerBcH <bernardoheuer2005@gmail.com>
2025-10-17 10:14:43 -0300

    Ajustes 2.0 17/10 - Atribuindo pasta de documentos e criando novos Aggregates no dominio-principal

---
+++ dominio-principal/src/main/java/dev/gestock/sge/dominio/principal/movimentacao/Movimentacao.java
@@ -0,0 +1,9 @@
+package dev.gestock.sge.dominio.principal.cliente;
+
+import static org.apache.commons.lang3.Validate.*;
+
+import dev.gestock.sge.dominio.principal.estoque.Estoque;
+
+import java.util.ArrayList;
+import java.util.List;
+
