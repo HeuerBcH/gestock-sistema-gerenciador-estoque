a9be1fd7151a859c594e42600d8b26e14d0a09f8
Rodrigo Nunes <nunes.rodrigo.dev@gmail.com>
2025-10-21 19:26:22 -0300

    Testes CalcularROPFeature e o EmitirAlertasEstoqueBaixoFeature

--- dominio-principal/src/test/java/dev/gestock/sge/dominio/principal/RunCucumberTest.java
+++ dominio-principal/src/test/java/dev/gestock/sge/dominio/principal/RunCucumberTest.java
@@ -5,13 +5,13 @@ import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("dev/gestock/sge/dominio/principal")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "dev.gestock.sge.dominio.principal")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, summary")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
+
+
